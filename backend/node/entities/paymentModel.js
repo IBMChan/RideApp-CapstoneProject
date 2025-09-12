30041ed850f08ada17161b4e60c45365c20e3921
shriya chouhan <shriya.chouhan@ibm.com>
2025-09-12 11:49:43 +0530

    Payment and ratings schema

---
+++ backend/node/entities/paymentModel.js
@@ -0,0 +1,39 @@
+import {Schema,model}  from "mongoose";
+
+const paymentSchema = new Schema({
+  payment_id: {
+    type: Number,      
+    default: function(){
+        const now= Date.now();
+        return now;
+    },
+    unique:true,
+    index: true
+  },
+  ride_id: {
+    type: Number,     // store MySQL rider_id
+    required: true
+  },
+  fare: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ["pending", "success", "failed", "refunded"], 
+    default: "pending"
+  },
+  mode: {
+    type: String,
+    enum: ["cash", "card", "upi", "wallet"],
+    required: true
+  },
+  Payed_At: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+const Payment = model("Payments", paymentSchema);
+
+export default Payment;
