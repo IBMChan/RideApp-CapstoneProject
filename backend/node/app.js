43c7361f7e7929f1898a0cecf7940b71561df2e2
Pratik Thakur <pratik.thakur1@ibm.com>
2025-09-12 12:24:47 +0530

    Updates in app.js

--- backend/node/app.js
+++ backend/node/app.js
@@ -8,23 +8,23 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 (async () => {
-  try {
-    // Authenticate DB connection
-    await sequelize.authenticate();
-    console.log("Database connection established successfully.");
+    try {
+        // Authenticate DB connection
+        await sequelize.authenticate();
+        console.log("Database connection established successfully.");
 
-    // Sync models
-    await sequelize.sync({ alter: true });
-    console.log("Models synced successfully.");
+        // Sync models
+        await sequelize.sync({ alter: true });
+        console.log("Models synced successfully.");
 
-    app.get("/", (req, res) => {
-      res.send("Sequelize is running and working fine");
-    });
+        app.get("/", (req, res) => {
+            res.send("Sequelize is running and working fine");
+        });
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
 })();
--- backend/node/config/sqlConfig.js
+++ backend/node/config/sqlConfig.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
+export const sequelize = new Sequelize(
   process.env.MYSQL_DB,
   process.env.MYSQL_USER,
   process.env.MYSQL_PASSWORD,
