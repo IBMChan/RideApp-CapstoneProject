e4c0545ba46cfa5c059ef101288a0182d28ad13c
Team4-commits <227581998+Team4-commits@users.noreply.github.com>
2025-09-13 20:32:46 +0530

    Update userModel.js

--- backend/node/entities/userModel.js
+++ backend/node/entities/userModel.js
@@ -1,3 +1,4 @@
+
 import { DataTypes } from "sequelize";
 import sequelize from "../config/sqlConfig.js";
 
@@ -13,6 +14,32 @@ const User = sequelize.define("User", {
   kyc_document: { type: DataTypes.STRING },
   emailVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
   phoneVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
+  status: { type: DataTypes.ENUM("active", "inactive"), defaultValue: "active" },
+  is_live_currently: { type: DataTypes.ENUM("yes", "no"),  defaultValue: "no",                 
+},
 });
 
 export default User;
+
+
+
+
+
+// import { DataTypes } from "sequelize";
+// import sequelize from "../config/sqlConfig.js";
+
+// const User = sequelize.define("User", {
+//   user_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+//   full_name: { type: DataTypes.STRING, allowNull: false },
+//   phone: { type: DataTypes.STRING, allowNull: false, unique: true },
+//   email: { type: DataTypes.STRING, allowNull: false, unique: true },
+//   role: { type: DataTypes.ENUM("driver", "rider", "admin"), allowNull: false },
+//   license: { type: DataTypes.STRING },
+//   password_hash: { type: DataTypes.STRING, allowNull: false },
+//   gender: { type: DataTypes.STRING },
+//   kyc_document: { type: DataTypes.STRING },
+//   emailVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
+//   phoneVerified: { type: DataTypes.BOOLEAN, defaultValue: false },
+// });
+
+// export default User;
